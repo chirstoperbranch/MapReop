78b68dd621a3ddceebee9ee2e4b50ec34a2ad16c
chirstoperbranch <crb32@students.ptcollege.edu>
2023-06-05 14:24:46 -0400

    SCAassignment

---
+++ SCAsassignment/app/src/main/java/com/example/scasassignment/MapsActivity.kt
@@ -0,0 +1,64 @@
+package com.example.scasassignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.scasassignment.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+    private fun launchTrackingService() {
+        RouteTrackingService.trackingCompletion.observe(this, Observer {       agentId ->
+            showResult("Agent $agentId arrived!")
+        })
+        val serviceIntent = Intent(this,       RouteTrackingService::class.java).apply {
+            putExtra(EXTRA_SECRET_CAT_AGENT_ID, "007")
+        }
+        ContextCompat.startForegroundService(this, serviceIntent)
+    }
+    workManager.getWorkInfoByIdLiveData(catSuitUpRequest.id)
+    .observe(this, Observer { info ->
+        if (info.state.isFinished) {
+            showResult("Agent done suiting up. Ready to go!")
+            launchTrackingService()
+        }
+    })
+}
